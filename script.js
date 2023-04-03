3adb4409a4ae704a47520987abe5b806bae55517
MD SHOHEL RANA <ranacseru37@gmail.com>
2023-04-03 19:43:52 +0600

    Exploring JS

---
+++ script.js
@@ -0,0 +1,16 @@
+var friendsAge = [125,125,36,45];
+
+var sonaliAge = friendsAge[2];
+
+// console.log(sonaliAge);
+
+var positon  = friendsAge.indexOf(141)
+
+// console.log(positon);
+
+friendsAge.push(455);
+
+console.log(friendsAge);
+
+friendsAge.pop();
+console.log(friendsAge);
